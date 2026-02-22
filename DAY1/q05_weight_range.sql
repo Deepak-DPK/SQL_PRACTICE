e72e4cc6529904e1b61a8e516cee12f82e84415d
Deepak <deepakramesh1583@gmail.com>
2026-02-22 22:39:34 +0530

    Add q05_weight_range.sql - weight range filter

---
+++ DAY1/q05_weight_range.sql
@@ -0,0 +1,13 @@
+-- 5. Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+
+SELECT first_name,last_name
+FROM patients
+where weight between 100 AND 120;  
+
+
+--or \./
+SELECT
+  first_name,
+  last_name
+FROM patients
+WHERE weight >= 100 AND weight <= 120;
