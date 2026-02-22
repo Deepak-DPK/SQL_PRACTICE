b241084c69f1122246bab5cbf21e5470f1dea979
Deepak <deepakramesh1583@gmail.com>
2026-02-22 22:39:27 +0530

    Add q02_fill_allergies.sql - allergy cleanup

---
+++ DAY1/q02_fill_allergies.sql
@@ -0,0 +1,5 @@
+-- 2. Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA’
+
+update patients
+SET allergies='NKA'
+where allergies is null;
